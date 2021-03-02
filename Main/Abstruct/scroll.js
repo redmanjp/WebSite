51dadecb29674faa80c573d4c26c7e87ee61d763
s1270049 <69134448+s1270049@users.noreply.github.com>
2021-03-02 19:29:31 +0900

    delete directory

--- Main/Abstruct/scroll.js
+++
@@ -1,7 +0,0 @@
-coast target = document.querySelector('.text');
-coast threshold = 200;
-window.addEventListener('scroll', () => {
-    if(window.innerHeight > target.getBoundingClientRect().top+threshold){
-        target.classList.remove('loading');
-    }
-});
