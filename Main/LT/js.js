24ac70714fe595be10b4c75eaf3a368d3f35493f
redmanjp <69310280+redmanjp@users.noreply.github.com>
2021-03-11 18:34:11 +0900

    Update js.js

--- Main/LT/js.js
+++ Main/LT/js.js
@@ -2,12 +2,12 @@ var count = 0;
 $("fire").on("mouseover",function(){
 $(this).fadeOut( 1000 );
 count++;
-　　　　console.log('ああ。');
-
+$('h2').text('こんにちは');
+ 
 if (count == 4) {
  
     // コンソールに県名を表示
-　　　　console.log('ああ。');
+$('h2').text('こんにちは');
  
 　　}
 
